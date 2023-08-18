6b441d2c85e7d185c380905c30f1ee61c92f9df5
katevi <catherine.vinnik@gmail.com>
2023-08-18 20:26:23 +0300

    Add archivation, group files with data to folders

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 	"path"
 	devicelister "scsicollector/internal"
 	"strings"
-
 	"time"
 )
 
 const (
-	//period = 10 * time.Minutes
-	period           = 1 * time.Second
+	//pollingPeriod = 10 * time.Minutes
+	pollingPeriod    = 1 * time.Second
+	timeout          = 3 * time.Second
 	collectedDataDir = "./data"
 	allDevicesKey    = ""
 	appName          = "scsi-data-collector"
@@ -37,16 +37,37 @@ func main() {
 	}
 
 	for {
-		time.Sleep(period)
+		time.Sleep(pollingPeriod)
+		timeNow := time.Now().UTC().Format("2006-01-02-15-04-05")
+		execDirPath := path.Join(collectedDataDir, timeNow)
+		if err := os.Mkdir(execDirPath, os.ModePerm); err != nil {
+			appLog.Printf("Failed to create dir %s", err)
+		}
+
 		blockDevices, _ := devicelister.GetBlockDevices(allDevicesKey)
+		resultsCh := make(chan struct{}, len(blockDevices))
 		for _, blockDevice := range blockDevices {
-			go execCommand(appLog, blockDevice)
+			go execCommand(appLog, resultsCh, execDirPath, blockDevice)
+		}
+
+		done := make(chan struct{})
+		go func() {
+			for i := 0; i < len(blockDevices); i++ {
+				<-resultsCh
+			}
+			done <- struct{}{}
+		}()
+		select {
+		case <-done:
+			archiveExecDir(appLog, path.Join(collectedDataDir, timeNow), execDirPath)
+		case <-time.After(timeout):
+			continue
 		}
 	}
 }
 
-func execCommand(appLog *log.Logger, device devicelister.BlockDevice) {
-	cmdArgs := fmt.Sprintf("sg_logs -a %s", device.Name)
+func execCommand(appLog *log.Logger, resultsCh chan<- struct{}, execDirPath string, device devicelister.BlockDevice) {
+	cmdArgs := fmt.Sprintf("sg_logs -aa %s", device.Name)
 	cmd := exec.Command("bash", "-c", cmdArgs)
 
 	var outb, errb bytes.Buffer
@@ -56,20 +77,29 @@ func execCommand(appLog *log.Logger, device devicelister.BlockDevice) {
 	if err != nil {
 		appLog.Printf("Cmd execution completed with err %s %v", err, errb.String())
 	}
-	deviceName := device.Name
-	deviceName = strings.ReplaceAll(deviceName, "/", "_")
-	filepath := path.Join(collectedDataDir, deviceName)
 
-	dumpDatatoFile(filepath,
+	deviceName := strings.ReplaceAll(device.Name, "/", "_")
+	filepath := path.Join(execDirPath, deviceName)
+	dumpCmdResult2File(filepath,
 		[]byte(fmt.Sprint("cmd:\n", cmdArgs)),
-		[]byte(fmt.Sprint("time:\n", time.Now().String())),
 		[]byte(fmt.Sprint("out:\n", outb.String())),
 		[]byte(fmt.Sprint("err:\n", errb.String())))
+	resultsCh <- struct{}{}
 }
 
-func dumpDatatoFile(filepath string, contents ...[]byte) {
+func archiveExecDir(appLog *log.Logger, archiveName, execDir string) {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("tar czf %s.tar.gz %s --remove-files", archiveName, execDir))
+	err := cmd.Run()
+	var errb bytes.Buffer
+	cmd.Stderr = &errb
+	if err != nil {
+		appLog.Printf("Cmd execution completed with err %s %v", err, errb.String())
+	}
+}
+
+func dumpCmdResult2File(path string, contents ...[]byte) {
 	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
